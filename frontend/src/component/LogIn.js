1b86131ec56a58957344d2be93646b2376f0c340
Prajwal Lokhande <104195092+PrajwalLokhande2003@users.noreply.github.com>
2024-11-19 13:23:01 +0530

    Update LogIn.js

--- frontend/src/component/LogIn.js
+++ frontend/src/component/LogIn.js
@@ -1,5 +1,5 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useEffect, useState } from "react";
+import React, {useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
