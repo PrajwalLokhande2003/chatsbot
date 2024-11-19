d2751e5dcbbae20436c1d340a4b3880d8745c896
Prajwal Lokhande <104195092+PrajwalLokhande2003@users.noreply.github.com>
2024-11-19 13:15:45 +0530

    Update Nav.js

--- frontend/src/component/Nav.js
+++ frontend/src/component/Nav.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useRef, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
