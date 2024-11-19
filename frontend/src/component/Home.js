09300a7beca730a3c38d9eb887b8433cfcc43a0e
Prajwal Lokhande <104195092+PrajwalLokhande2003@users.noreply.github.com>
2024-11-19 13:05:42 +0530

    Update Home.js

--- frontend/src/component/Home.js
+++ frontend/src/component/Home.js
@@ -62,6 +62,7 @@ const Home = () => {
         if(groups === ''){
             getGroups()
         }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [groups])
 
     useEffect(() => {
