233d9156bae74d7a0270620d3cc44e4cafd013a5
Prajwal Lokhande <104195092+PrajwalLokhande2003@users.noreply.github.com>
2024-11-19 19:40:19 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const server = require('http').createServer(app)
 const app = express();
 const cors = require('cors')
 require('./DB/config')
 const PORT = process.env.PORT || 5000
-const io = require('socket.io')(PORT,{
+const io = require('socket.io')(server,{
     cors: {
         origin: '*'
 })
@@ -256,5 +257,5 @@ app.delete('/exit-from-group/:id',async(req,res)=>{
 
 
 
-app.listen(PORT);
+server.listen(PORT);
 
