6175421cd9b58d7db6b963e3aa13a523ea69b5ab
Prajwal Lokhande <104195092+PrajwalLokhande2003@users.noreply.github.com>
2024-11-19 19:42:48 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,7 @@ const PORT = process.env.PORT || 5000
 const io = require('socket.io')(server,{
     cors: {
         origin: '*'
+    }
 })
 
 const { getDownloadURL, ref, getStorage, uploadBytes } = require('firebase/storage')
